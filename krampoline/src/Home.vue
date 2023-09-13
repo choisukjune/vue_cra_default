ca4c413a2c8cdbe9fa633bca3f9e3b7b598564f9
sukjune.choi <sukjune.choi@gmail.com>
2023-09-13 18:17:03 +0900

    test

--- krampoline/src/Home.vue
+++ krampoline/src/Home.vue
@@ -1,8 +1,18 @@
-const date = () =>{
-  return new Date().toDateString()
-}
-
+<script>
+export default {
+  methods: {
+    currentDateTime() {
+      const current = new Date();
+      const date = current.getFullYear()+'-'+(current.getMonth()+1)+'-'+current.getDate();
+      const time = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+      const dateTime = date +' '+ time;
+       
+      return dateTime;
+    }
+  }
+};
+</script>
 <template>
   <h1>Home - VUE - CRA</h1>
-  <h3>{date()}</h3>
+  <h3>Current Date & Time: {{currentDateTime()}}</h3>
 </template>
