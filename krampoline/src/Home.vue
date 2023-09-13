95a7cb8ab9aa1c73814a81df570131d3fbc7e341
sukjune.choi <sukjune.choi@gmail.com>
2023-09-13 18:39:35 +0900

    test

--- krampoline/src/Home.vue
+++ krampoline/src/Home.vue
@@ -8,11 +8,16 @@ export default {
       const dateTime = date +' '+ time;
        
       return dateTime;
+    },
+    setTimeoutDateTime(){
+      setTimeout(()=>{
+         this.currentDateTime()
+      },1000);
     }
   }
 };
 </script>
 <template>
   <h1>Home - VUE - CRA</h1>
-  <h3>Current Date & Time: {{setTimeout(function(){currentDateTime();},1000)}</h3>
+  <h3>Current Date & Time: {{setTimeoutDateTime())}</h3>
 </template>
