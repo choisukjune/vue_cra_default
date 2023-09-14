e65f615243d6555c61f195514cab0ea6aae14c7b
choisukjune <sukjune.choi@gmail.com>
2023-09-14 11:04:09 +0900

    test

--- krampoline/src/Home.vue
+++ krampoline/src/Home.vue
@@ -1,23 +1,33 @@
 <script>
 export default {
+  data() {
+    return {
+        now: ""
+    }
+  },
   methods: {
     currentDateTime() {
-      const current = new Date();
-      const date = current.getFullYear()+'-'+(current.getMonth()+1)+'-'+current.getDate();
-      const time = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
-      const dateTime = date +' '+ time;
-       
-      return dateTime;
-    },
-    setTimeoutDateTime(){
-      setTimeout(()=>{
-         this.currentDateTime()
+      var _this = this;
+
+
+      setInterval(()=>{
+      
+        var current = new Date();
+        var date = current.getFullYear()+'-'+(current.getMonth()+1)+'-'+current.getDate();
+        var time = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+        var dateTime = date +' '+ time;
+        
+        _this.now = dateTime;
+
       },1000);
-    }
+    },
   }
+  , mounted(){
+      this.currentDateTime();
+    }
 };
 </script>
 <template>
   <h1>Home - VUE - CRA</h1>
-  <h3>Current Date & Time: {{setTimeoutDateTime()}}</h3>
+  <h3>Current Date & Time: {{now}}</h3>
 </template>
