77fda3b854eb66c928bf8582fee3ae433636a2f7
sukjune.choi <sukjune.choi@gmail.com>
2023-09-16 20:38:35 +0900

    test

--- krampoline/vue.config.js
+++ krampoline/vue.config.js
@@ -5,5 +5,5 @@ console.log(process.env.VUE_APP_A)
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath : `/${process.env.VUE_APP_A}/`,
-  outputDir: `./dist/`,
+  outputDir: `./dist/${process.env.VUE_APP_A}/`,
 })
